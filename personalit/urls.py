51c15ae973c8beee529bc740f48e1b693aea073f
CloverJoy <chris.ang.dev@gmail.com>
2021-03-12 22:32:45 -0800

    Added homepage

--- personalit/urls.py
+++ personalit/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api.models import PersonalityResource
+from . import views
 
 personality_resource = PersonalityResource()
 
 urlpatterns = [
+    path('', views.home),
     path('admin/', admin.site.urls),
     path('personalities/', include('personalities.urls')),
     path('api/', include(personality_resource.urls))
