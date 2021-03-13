b332b5e18b47b61dc3ab3d8def9927d792cd9a47
CloverJoy <chris.ang.dev@gmail.com>
2021-03-12 19:43:29 -0800

    Add models and migration

--- personalities/models.py
+++ personalities/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Classification(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Personality(models.Model):
+    mbtitype = models.CharField(max_length=4)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    classification = models.ForeignKey(
+        Classification, on_delete=models.CASCADE)
